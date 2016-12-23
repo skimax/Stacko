9ef1287cca18febd64d7ff1d9ecf9115f081da2f
skimax <pascou7@gmail.com>
2016-12-22 23:34:09 -0800

    lets save them

---
+++ app/app.js
@@ -0,0 +1,178 @@
+$(document).ready( function() {
+	$('.unanswered-getter').submit( function(event){
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		var tags = $(this).find("input[name='tags']").val();
+		getUnanswered(tags);
+	});
+
+	$('.inspiration-getter').submit( function(event){
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		var searchflow = $(this).find("input[name='answerers']").val();
+		getinspiration(searchflow);
+	});
+});
+
+// this function takes the question object returned by StackOverflow 
+// and creates new result to be appended to DOM
+var showQuestion = function(question) {
+	
+	// clone our result template code
+	var result = $('.templates .question').clone();
+	
+	// Set the question properties in result
+	var questionElem = result.find('.question-text a');
+	questionElem.attr('href', question.link);
+	questionElem.text(question.title);
+
+	// set the date asked property in result
+	var asked = result.find('.asked-date');
+	var date = new Date(1000*question.creation_date);
+	asked.text(date.toString());
+
+	// set the #views for question property in result
+	var viewed = result.find('.viewed');
+	viewed.text(question.view_count);
+
+	// set some properties related to asker
+	var asker = result.find('.asker');
+	asker.html('<p>Name: <a target="_blank" href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
+													question.owner.display_name +
+												'</a>' +
+							'</p>' +
+ 							'<p>Reputation: ' + question.owner.reputation + '</p>'
+	);
+
+	return result;
+};
+
+
+var showAnswer = function(answer){
+  
+  var resultshow = $('.templates .trueAnswer').clone();
+  
+  resultshow.find('.profile-img img').attr('src', answer.user.profile_image);
+
+  
+    var Answerer = resultshow.find('.Answername');
+	 Answerer.text(answer.user.display_name);
+
+	 var Reputation = resultshow.find('.reputation');
+	 Reputation.text(answer.user.reputation);
+
+	 var answerersLink = resultshow.find('.answersLink a');
+	 answerersLink.attr("href" , answer.user.link);
+	 answerersLink.text(answer.user.link);
+
+
+  return resultshow;
+
+}
+
+
+
+
+
+
+
+// this function takes the results object from StackOverflow
+// and creates info about search results to be appended to DOM
+var showSearchResults = function(query, resultNum) {
+	var results = resultNum + ' results for <strong>' + query;
+	return results;
+};
+
+// takes error string and turns it into displayable DOM element
+var showError = function(error){
+	var errorElem = $('.templates .error').clone();
+	var errorText = '<p>' + error + '</p>';
+	errorElem.append(errorText);
+};
+
+// takes a string of semi-colon separated tags to be searched
+// for on StackOverflow
+var getUnanswered = function(tags) {
+	
+	// the parameters we need to pass in our request to StackOverflow's API
+	var request = {tagged: tags,
+								site: 'stackoverflow',
+								order: 'desc',
+								sort: 'creation'};
+	
+	var result = $.ajax({
+		url: "http://api.stackexchange.com/2.2/questions/unanswered",
+		data: request,
+		dataType: "jsonp",
+		type: "GET",
+		})
+	.done(function(result){
+		var searchResults = showSearchResults(request.tagged, result.items.length);
+
+		$('.search-results').html(searchResults);
+
+		$.each(result.items, function(i, item) {
+			var question = showQuestion(item);
+			$('.results').append(question);
+		});
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
+
+
+
+
+// this function takes the results object from StackOverflow
+// and creates info about search results to be appended to DOM
+var showSearchResults = function(query, resultNum) {
+	var results = resultNum + ' results for <strong>' + query;
+	return results;
+};
+
+// takes error string and turns it into displayable DOM element
+var showError = function(error){
+	var errorElem = $('.templates .error').clone();
+	var errorText = '<p>' + error + '</p>';
+	errorElem.append(errorText);
+};
+
+var getinspiration = function(searchflow) {
+	
+	// the parameters we need to pass in our request to StackOverflow's API
+	var request = {tag: searchflow,
+
+                        site: 'stackoverflow',
+                        period: 'all_time'};
+
+
+	var result = $.ajax({
+		url: "http://api.stackexchange.com/2.2/tags/" +searchflow+ "/top-answerers/all_time",
+		data: request,
+		dataType: "jsonp",
+		type: "GET",
+		})
+	.done(function(result){
+		var searchResults = showSearchResults(request.tag, result.items.length);
+		$('.search-results').html(searchResults);
+           console.log(result);
+
+	 $.each(result.items, function(i, item) {
+		 console.log(item);
+		 var respond = showAnswer(item);
+	  $('.results').append(respond);
+
+
+
+		});
+	})
+	.fail(function(jqXHR, error, errorThrown){
+
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
